18811b8f5ae354b54415d23801d45f3369fd7720
valdean132 <65917804+valdean132@users.noreply.github.com>
2021-06-01 23:46:32 -0400

    1.0.1.1.9
    Dando continuidade ao formulário de Agendamentos

--- fomulario/painel/pages/home.php
+++ fomulario/painel/pages/home.php
@@ -85,40 +85,91 @@
 </div><!-- Home -->
 
 <div class="popup">
-    <div class="form-agend">
-        <a href="" class="sairModal">x</a><!-- Fechar Popup -->
-        <h2>Agendamento de Valdean Souza</h2>
-        <form method="POST">
-            <div class="center-form">
-                <div class="form-50">
-                    <div class="form-group-agend disabled">
-                        <label for="nome<?php echo $key+1; ?>">Nome:</label>
-                        <input type="text" name="" disabled value="Valdean Souza">
-                        <input type="hidden" id="nome<?php echo $key+1; ?>" name="nome" required value="<?php echo $value['nome'];?>">
-                    </div><!-- Form-Group-agend-Nome -->
+    <?php foreach($porAgendar as $key => $value){ ?>
+        <div class="form-agend">
+            <a href="" class="sairModal">x</a><!-- Fechar Popup -->
+            <h2>Agendamento de <?php echo $value['nome'];?></h2>
+            <form method="POST">
+                <div class="center-form">
+                    <div class="form-50">
+                        <div class="form-group-agend disabled">
+                            <label for="nome<?php echo $key+1; ?>">Nome:</label>
+                            <input type="text" name="" disabled value="<?php echo $value['nome'];?>">
+                            <input type="hidden" id="nome<?php echo $key+1; ?>" name="nome" required value="<?php echo $value['nome'];?>">
+                        </div><!-- Form-Group-agend-Nome -->
+        
+                        <div class="form-group-agend disabled">
+                            <label for="email<?php echo $key+1; ?>">E-mail:</label>
+                            <input type="email" name="" disabled value="<?php echo $value['email'];?>">
+                        </div><!-- Form-Group-agend-Email -->
+        
+                        <div class="form-group-agend disabled">
+                            <label for="telefone<?php echo $key+1; ?>">Celular:</label>
+                            <input type="text" name="" disabled value="<?php echo $value['telefone'];?>">
+                        </div><!-- Form-Group-agend-Telefone -->
+        
+                        <div class="form-group-agend disabled">
+                            <label for="endereco<?php echo $key+1; ?>">Endereço:</label>
+                            <input type="text" name="" disabled value="<?php echo $value['endereco'];?>">
+                        </div><!-- Form-Group-agend-Telefone -->
+                        
+                        <div class="form-group-agend ged disabled">
+                            <?php if($value['resp_ged'] == 'Sim'){?>
+                                <label for="">Ged:</label>
+                                <input type="text" name="" disabled value="<?php echo $value['nome_ged']; ?>">
+            
+                                <label for="endereco<?php echo $key+1; ?>">Supervisor:</label>
+                                <input type="text" name="" disabled value="<?php echo $value['nome_supervisor']; ?>">
+                            <?php }else{?>
+                                <div class="null-ged">
+                                    <p>Não Possue Ged</p>
+                                </div>
+                            <?php } ?>
+                        </div><!-- Form-Group-agend-Ged -->
+                    </div><!-- Form-50 -->
 
-                    <div class="form-group-agend disabled">
-                        <label for="email<?php echo $key+1; ?>">E-mail:</label>
-                        <input type="email" name="" disabled value="valdeanpds@gmail.com">
-                        <input type="hidden" id="email<?php echo $key+1; ?>" name="email" required value="<?php echo $value['nome'];?>">
-                    </div><!-- Form-Group-agend-Email -->
+                    <div class="form-50">
+                        <div class="form-group-agend disabled">
+                            <label for="">Tipos de Ajuda</label>
+                            <?php 
+                                for($i = 1; $i <= 6; $i++){
+                                    if($value["tipo_ajuda_opcao{$i}"] == ''){ 
+                            ?>
+                                        <div class="tipo-ajuda"></div><!-- Tipo Ajuda -->
+                            <?php }else{ ?>
+                                        <div class="tipo-ajuda">
+                                            <div class="tipo-ajuda-wrapper">
+                                                <div class="checkbox">
+                                                    <i><?php echo Icon::$vistos2; ?></i>
+                                                </div>
+                                                <label for=""><?php echo $value["tipo_ajuda_opcao{$i}"] ?></label>
+                                            </div><!-- Tipo Ajuda Wrapper -->
+                                        </div><!-- Tipo Ajuda -->
+                            <?php
+                                    }
+                                } 
+                            ?>
+                        </div><!-- Form-Group-agend-Tipo-Ajuda -->
 
-                    <div class="form-group-agend disabled">
-                        <label for="telefone<?php echo $key+1; ?>">Celular:</label>
-                        <input type="text" name="" disabled value="(92) 9 99296-1661">
-                        <input type="hidden" id="telefone<?php echo $key+1; ?>" name="telefone" required value="<?php echo $value['nome'];?>">
-                    </div><!-- Form-Group-agend-Email -->
+                        <div class="form-group-agend necessidade disabled">
+                            <?php if($value['necessidade'] == ''){?>
+                                <div class="null-necessidade">
+                                    <p>Não Possue necessidade</p>
+                                </div>
+                            <?php }else{?>
+                                <label for="">Necessidade:</label>
+                                <h4><?php echo $value['necessidade']; ?></h4>
+                            <?php } ?>
+                        </div><!-- Form-Group-agend-Ged -->
 
-                </div><!-- Form-50 -->
-                <div class="form-50">
-                    <div class="form-group-agend">
-                        <label for="nome<?php echo $key+1; ?>">Nome:</label>
-                        <input type="text" name="" value="Valdean Souza">
-                        <input type="hidden" id="nome<?php echo $key+1; ?>" name="nome" required value="<?php echo $value['nome'];?>">
-                    </div><!-- Form-Group-agend-Nome -->
-                </div><!-- Form-50 -->
-            </div>
-            <input type="submit" value="Agendar" name="acao">
-        </form><!-- Form -->
-    </div><!-- Form Agend -->
+                        <div class="form-group-agend necessidade">
+                            <label for="data-agend5">Data de Consulta:</label>
+                            <input type="datetime-local" name="data-agend" id="data-agend5">
+                        </div><!-- Form-Group-data-agend -->
+                    </div><!-- Form-50 -->
+                </div>
+                <input type="submit" value="Agendar" name="acao">
+            </form><!-- Form -->
+        </div><!-- Form Agend -->
+    <?php } ?>
 </div><!-- Popup -->
